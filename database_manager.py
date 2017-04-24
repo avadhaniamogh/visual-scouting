fe6d87fd1320b17f96709cb335c40329f7cd085c
Amogh Avadhani <avadhaniamogh@bitbucket.org>
2017-04-24 14:49:11 -0400

    Overall, home and away stats for teams in a particular season

--- database_manager.py
+++ database_manager.py
@@ -1,8 +1,53 @@
 import sqlite3
+import json
 
 conn = sqlite3.connect('database.sqlite')
 cursor = conn.cursor()
 
+# Returned dictionary is of the form <TeamId, [Position, Points, Wins, Draws, Losses, GF, GA, GD, CS, TeamId, Long name, short name]>
+def getStandings(league_id, season):
+    get_standings_query = "Select home_team_goal, away_team_goal, home_team_api_id, away_team_api_id From Match Where season = ? And league_id = ?;";
+    all_standings = {}
+
+    cursor.execute(get_standings_query, (season, league_id,))
+    for row in cursor:
+        homeTeamGoal = row[0]
+        awayTeamGoal = row[1]
+        homeTeamId = row[2]
+        awayTeamId = row[3]
+
+        print homeTeamGoal
+
+        if homeTeamId not in all_standings:
+            all_standings[homeTeamId] = [0] * 12
+        if awayTeamId not in all_standings:
+            all_standings[awayTeamId] = [0] * 12
+
+        home_team_result_list = all_standings[homeTeamId]
+        away_team_result_list = all_standings[awayTeamId]
+
+        home_team_result_list[9] = homeTeamId
+        away_team_result_list[9] = awayTeamId
+
+        if homeTeamGoal > awayTeamGoal:
+            home_team_result_list[2] += 1  # win for home team
+            home_team_result_list[1] += 3  # 3 points for home team
+            away_team_result_list[4] += 1  # loss for away team
+        elif awayTeamGoal == homeTeamGoal:
+            home_team_result_list[3] += 1  # draw for home team
+            home_team_result_list[1] += 1  # 1 point for home team
+
+            away_team_result_list[3] += 1  # draw for away team
+            away_team_result_list[1] += 1  # 1 point for away team
+        else:
+            home_team_result_list[4] += 1  # loss for home team
+            away_team_result_list[2] += 1  # win for away team
+            away_team_result_list[1] += 3  # 3 points for away win
+
+        all_standings[homeTeamId] = home_team_result_list
+        all_standings[awayTeamId] = away_team_result_list
+
+    print all_standings
 
 def getAllCountries():
     query = "Select * from Country"
@@ -40,10 +85,27 @@ def getTeamsForSeason(league_id, season):
     return teams
 
 
-# Returned dictionary is of the form <TeamId, [Points, Wins, Draws, Losses, GF, GA, GD]>
+def getTeamLongAndShortNames(team_id):
+    get_team_name_query = "Select team_long_name, team_short_name From Team Where team_api_id = ?;";
+    cursor.execute(get_team_name_query, (team_id,))
+    team_names = []
+    for row in cursor:
+        team_names.append(row[0])
+        team_names.append(row[1])
+    return team_names
+
+
+# Returned dictionary is of the form <TeamId, [Position, Points, Wins, Draws, Losses, GF, GA, GD, CS, TeamId, Long name, short name]>
 def getStandingsOfTeamForSeason(league_id, season):
     get_standings_query = "Select home_team_goal, away_team_goal, home_team_api_id, away_team_api_id From Match Where season = ? And league_id = ?;";
     all_standings = {}
+    home_standings = {}
+    away_standings = {}
+
+    overall = []
+    home = []
+    away = []
+
     cursor.execute(get_standings_query, (season, league_id,))
     for row in cursor:
         homeTeamGoal = row[0]
@@ -52,44 +114,170 @@ def getStandingsOfTeamForSeason(league_id, season):
         awayTeamId = row[3]
 
         if homeTeamId not in all_standings:
-            all_standings[homeTeamId] = [0] * 7
+            all_standings[homeTeamId] = [0] * 12
         if awayTeamId not in all_standings:
-            all_standings[awayTeamId] = [0] * 7
+            all_standings[awayTeamId] = [0] * 12
+
+        if homeTeamId not in home_standings:
+            home_standings[homeTeamId] = [0] * 12
+
+        if awayTeamId not in away_standings:
+            away_standings[awayTeamId] = [0] * 12
 
         home_team_result_list = all_standings[homeTeamId]
         away_team_result_list = all_standings[awayTeamId]
 
+        only_home_result = home_standings[homeTeamId]
+        only_away_result = away_standings[awayTeamId]
+
+        home_team_result_list[9] = homeTeamId
+        away_team_result_list[9] = awayTeamId
+
+        only_home_result[9] = homeTeamId
+        only_away_result[9] = awayTeamId
+
         if int(homeTeamGoal) > int(awayTeamGoal):
-            home_team_result_list[1] += 1  # win for home team
-            home_team_result_list[0] += 3  # 3 points for home team
-            away_team_result_list[3] += 1  # loss for away team
-        elif int(awayTeamGoal) > int(homeTeamGoal):
-            home_team_result_list[2] += 1  # draw for home team
-            home_team_result_list[0] += 1  # 1 point for home team
+            home_team_result_list[2] += 1  # win for home team
+            only_home_result[2] += 1
+            home_team_result_list[1] += 3  # 3 points for home team
+            only_home_result[1] += 3
+            away_team_result_list[4] += 1  # loss for away team
+            only_away_result[4] += 1
+        elif int(awayTeamGoal) == int(homeTeamGoal):
+            home_team_result_list[3] += 1  # draw for home team
+            only_home_result[3] += 1
+            home_team_result_list[1] += 1  # 1 point for home team
+            only_home_result[1] += 1
 
-            away_team_result_list[2] += 1  # draw for away team
-            away_team_result_list[0] += 1  # 1 point for away team
+            away_team_result_list[3] += 1  # draw for away team
+            only_away_result[3] += 1
+            away_team_result_list[1] += 1  # 1 point for away team
+            only_away_result[1] += 1
         else:
-            home_team_result_list[3] += 1  # loss for home team
+            home_team_result_list[4] += 1  # loss for home team
+            only_home_result[4] += 1
 
-            away_team_result_list[1] += 1  # win for away team
-            away_team_result_list[0] += 3  # 3 points for away win
+            away_team_result_list[2] += 1  # win for away team
+            only_away_result[2] += 1
+            away_team_result_list[1] += 3  # 3 points for away win
+            only_away_result[1] += 3
 
-            home_team_result_list[4] += int(homeTeamGoal)  # GF
-            home_team_result_list[5] += int(awayTeamGoal)  # GA
-            home_team_result_list[6] += (int(homeTeamGoal) - int(awayTeamGoal))
+        home_team_result_list[5] += int(homeTeamGoal)  # GF
+        home_team_result_list[6] += int(awayTeamGoal)  # GA
+        home_team_result_list[7] += (int(homeTeamGoal) - int(awayTeamGoal))
 
-        away_team_result_list[4] += int(awayTeamGoal)  # GF
-        away_team_result_list[5] += int(homeTeamGoal)  # GA
-        away_team_result_list[6] += int(awayTeamGoal) - int(homeTeamGoal)
+        only_home_result[5] += int(homeTeamGoal)
+        only_home_result[6] += int(awayTeamGoal)
+        only_home_result[7] += (int(homeTeamGoal) - int(awayTeamGoal))
+
+        if int(homeTeamGoal) == 0:
+            home_team_result_list[8] += 1
+            only_home_result[8] += 1
+
+        if int(awayTeamGoal) == 0:
+            away_team_result_list[8] += 1
+            only_away_result[8] += 1
+
+        away_team_result_list[5] += int(awayTeamGoal)  # GF
+        away_team_result_list[6] += int(homeTeamGoal)  # GA
+        away_team_result_list[7] += int(awayTeamGoal) - int(homeTeamGoal)
+
+        only_away_result[5] += int(awayTeamGoal)
+        only_away_result[6] += int(homeTeamGoal)
+        only_away_result[7] += (int(awayTeamGoal) - int(homeTeamGoal))
 
         all_standings[homeTeamId] = home_team_result_list
         all_standings[awayTeamId] = away_team_result_list
 
-    return all_standings
+        home_standings[homeTeamId] = only_home_result
+        away_standings[awayTeamId] = only_away_result
+
+    sorted_all_standings = sorted(all_standings.items(), key=lambda k: (k[1][1], k[1][7]), reverse=True)
+    print sorted_all_standings
+    all_position = []
+    for club in sorted_all_standings:
+        all_position.append(club[0])
+
+    for key, value in all_standings.iteritems():
+        # Add position to each club
+        pos = all_position.index(key)
+        value[0] = pos + 1
+
+        # Team names
+        team_names = getTeamLongAndShortNames(key)
+        value[10] = str(team_names[0])
+        value[11] = str(team_names[1])
+
+    print all_standings
+
+    sorted_home_standings = sorted(home_standings.items(), key=lambda k: (k[1][1], k[1][7]), reverse=True)
+    home_position = []
+    for club in sorted_home_standings:
+        home_position.append(club[0])
+    # print home_standings
+    # print home_position
+
+    for key, value in home_standings.iteritems():
+        pos = home_position.index(key)
+        value[0] = pos + 1
+
+        # Team names
+        team_names = getTeamLongAndShortNames(key)
+        value[10] = str(team_names[0])
+        value[11] = str(team_names[1])
+
+    # print home_standings
+
+    sorted_away_standings = sorted(away_standings.items(), key=lambda k: (k[1][1], k[1][7]), reverse=True)
+    away_position = []
+    for club in sorted_away_standings:
+        away_position.append(club[0])
+
+    for key, value in away_standings.iteritems():
+        pos = away_position.index(key)
+        value[0] = pos + 1
+
+        # Team names
+        team_names = getTeamLongAndShortNames(key)
+        value[10] = str(team_names[0])
+        value[11] = str(team_names[1])
+
+    # print away_standings
+
+    attributes_names_list = ['Pos', 'Pts', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'CS', 'team_id', 'long_name', 'short_name']
+    for key, value in all_standings.iteritems():
+        dict_list = zip(attributes_names_list, value)
+        dict_list = dict(dict_list)
+        overall.append(dict_list)
+
+    # print overall
+
+    for key, value in home_standings.iteritems():
+        dict_list = zip(attributes_names_list, value)
+        dict_list = dict(dict_list)
+        home.append(dict_list)
+
+    for key, value in away_standings.iteritems():
+        dict_list = zip(attributes_names_list, value)
+        dict_list = dict(dict_list)
+        away.append(dict_list)
+
+    data = {}
+    data['overall'] = overall
+    data['home'] = home
+    data['away'] = away
+    json_data = json.dumps(data)
+    print json_data
+
+    # return all_standings
 
 
 # teams = getTeamsForSeason("1729", "2014/2015")
 # print teams
 
-# getStandingsOfTeamForSeason("1729", "2014/2015")
+all_standings = getStandingsOfTeamForSeason("1729", "2014/2015")
+# print sorted(all_standings.items(), key=lambda k: (k[1][1], k[1][7]), reverse=True)
+
+# getTeamLongAndShortNames("9825")
+
+# getStandings("1729", "2014/2015")
